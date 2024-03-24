d2aa90a8d8287ace9892399c116977a7d33cef8f
mrhidethepain <157283691+mrhidethepain@users.noreply.github.com>
2024-03-24 10:10:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,77 +1,126 @@
-import sys
-# try:
-#     a=int(input())
-#     b=int(input())
-#     result=a/b
-# # except ZeroDivisionError:
-# #     print("Error")
-# # except ValueError:
-# #     print("Error: wrong value")
-# except Exception:
-#     print("dupa")
-# else:
-#     print(result)
-# l1=[1,2,3,4,5,6,7,8,9]
-# l2=[]
-# for i in l1:
-#     l2.append(i**2)
-# print(l2)
-# l2=[i**2 for i in l1]
-# print(l2)
-# l3=[3**y for y in range(1,6)]
-# print(l3)
-# l4=[x for x in l1 if x%2==1]
-# print(l4)
-# l5=[(x,y) for x in l2 for y in l3]
-# print(l5)
-# l6=[]
-# for i in l2:
-#     for j in l3:
-#         l6.append((i,j))
-# print(l6)
-# s1={1:2,2:3,3:4,4:5,5:6,6:7,7:8,8:9}
-# s2={}
-# for key, value in s1.items():
-#     s2[value]=key
-# print(s2)
-# s3={v:k for k, v in s1.items()}
-# print(s3)
-import math
-# def rownanie_kwadratowe(a,b,c):
-#     delta= b**2-4*a*c
-#     if delta<0:
-#         print("Brak pierwiatkow")
-#         return 0
-#     elif delta==0:
-#        print("1 pierwiastek")
-#        x=(-b)/(2*a)
-#        return x
-#     elif delta>0:
-#         print("2 pierwiastki")
-#         x1 = (-b+math.sqrt(delta)) / (2 * a)
-#         x2 = (-b - math.sqrt(delta)) / (2 * a)
-#         return x1,x2
-# print(rownanie_kwadratowe(6,3,1))
-# print(rownanie_kwadratowe(1,3,1))
-# print(rownanie_kwadratowe(2,5,2))
-# def dlugosc_odcinak(x1=1,x2=2,y1=3,y2=4):
-#     return math.sqrt((x1-x2)**2+(y1-y2)**2)
-# print(dlugosc_odcinak())
-# print(dlugosc_odcinak(5,3,6,8))
-# # print(dlugosc_odcinak(1,5,8,3))
-# plik = open('tekst.txt','a+',encoding='utf-8')
-# # znak=plik.read(10)
-# linia=plik.readline()
-# linie=plik.readlines()
-# plik.close()
-# print(znak)
-# print(linia)
-# print(linie)
-# plik.write('aaaaaaaaaaaaa')
-# plik.seek(105)
-# zanki=plik.read(10)
-# plik.close()
-# print(zanki)
-# with open('tekst.txt','r') as plik:
-#     znaki= plik.read(10)
-# print(znaki)
+import numpy as np
+# a=np.array([0,1])
+# print(a)
+# a=np.arange(2)
+# print(a)
+# print(type(a))
+# print(a.dtype)
+# a=np.arange(2,dtype='int32')
+# print(a.dtype)
+#
+# b=a.astype('float')
+# print(b)
+# print(b.dtype)
+# print(b.shape)
+#
+# print(a.ndim)
+# m=np.array((np.arange(2),np.arange(2)))
+# print(m.shape)
+# print(m.shape)
+# print(m.ndim)
+# print(m)
+#
+# macierz=np.array([[1,2],[3,4],[5,6]])
+# print(macierz)
+# print(macierz.shape)
+# print(macierz.ndim)
+#
+#
+# zera=np.zeros((5,5))
+# jedynki=np.ones((4,4))
+# print(zera)
+# print(zera.dtype)
+# print(jedynki)
+# print(jedynki.dtype)
+#
+# pusta=np.empty((2,2))
+# print(pusta)
+# poz1=pusta[1,1]
+# poz2=pusta[0,1]
+# print(poz1)
+# print(poz2)
+#
+#
+# macierz=np.array([[1,2],[3,4]])
+# print(macierz)
+# liczby=np.arange(1,2,0.1)
+# print(liczby)
+#
+# liczby_lin=np.linspace(1,2,5,endpoint=False)
+# print(liczby_lin)
+
+# z=np.indices((5,3))
+# print(z)
+# print(z[1][2][1])
+# print(z.ndim)
+#
+# x,y=np.mgrid[0:5,0:5]
+# print(x)
+# print(y)
+
+# mat_diag_k=np.diag([a for a in range(5)],k=1)
+# print(mat_diag_k)
+#
+# z=np.fromiter(range(5),dtype='int32')
+# print(z)
+
+# znaki=b'abcdef'
+# zn1=np.frombuffer(znaki, dtype='S1')
+# zn2=np.frombuffer(znaki, dtype='S2')
+# print(zn1)
+# print(zn2)
+
+# zanki='abcdef'
+# zn3=np.array(list(zanki))
+# zn4=np.array(list(zanki),dtype='S1')
+# zn5=np.array(list(b'abcdef'))
+# zn6=np.fromiter(zanki,dtype='S1')
+# zn7=np.fromiter(zanki,dtype='U1')
+# print(zn3)
+# print(zn4)
+# print(zn5)
+# print(zn6)
+# print(zn7)
+
+# mat=np.ones((2,2))
+# mat_1=np.array([[3,4],[5,6]])
+# mat=mat+mat_1
+# # print(mat)
+# # print(mat-mat_1)
+# # print(mat/mat_1)
+# # print(mat*mat_1)
+#
+# a=np.dot(mat,mat_1)
+# print(a)
+# b=mat.dot(mat_1)
+# print(b)
+
+# a=np.arange(10)
+# print(a)
+# s=slice(2,7,2)
+# print(a[s])
+# s=range(2,7,2)
+# print(a[s])
+# print(a[2:7:2])
+# print(a[1:])
+# print(a[2:5])
+
+
+# mat=np.arange(25)
+# mat=mat.reshape((5,5))
+#
+# print(mat[1:])
+# print(mat[:,1])
+# print(mat[:,-1:])
+# print(mat[2:6,1:3])
+# print(mat[:,range(2,6,2)])
+# print(' ')
+
+# x=np.array([[0,1,2],[3,4,5],[6,7,8],[9,10,11]])
+# print(x)
+# rows=np.array([[0,0],[3,3]])
+# cols=np.array([[0,2],[0,2]])
+# y=x[rows,cols]
+# print(y)
+
+
---
+++ zadania.py
@@ -0,0 +1,4 @@
+import numpy as np
+a=np.arange(3,46,3)
+print(a)
+b=np.array([])
